12c13343fe4cbde04b921b2c166f0106d7c54b36
Sergey <domarch388640@gmail.com>
2019-08-13 10:15:50 +0300

    commit

--- moduleFirst.js/task05.js
+++ moduleFirst.js/task05.js
@@ -1,43 +1,40 @@
 "use strict";
 
-const deliveryCountry = prompt("Укажите в какую страну будет доставка!");
+let deliveryCountry = prompt("Укажите в какую страну будет доставка!");
 // console.log(deliveryCountry);
 
-const countryChina = "Китай";
-const countryChili = "Чили";
-const countryAustralia = "Австралия";
-const countryIndia = "Индия";
-const countryJamaica = "Ямайка";
+const countryChina = "китай";
+const countryChili = "чили";
+const countryAustralia = "австралия";
+const countryIndia = "индия";
+const countryJamaica = "ямайка";
 
-const priceChina = "100 кредитов";
-const priceChili = "250 кредитов";
-const priceAustralia = "170 кредитов";
-const priceIndia = "80 кредитов";
-const priceJamaica = "120 кредитов";
+const priceChina = 100;
+const priceChili = 250;
+const priceAustralia = 170;
+const priceIndia = 80;
+const priceJamaica = 120;
 
 if (deliveryCountry === null) {
-  console.log("В какую страну сделать доставку!");
+  console.log("Выберите страну!");
 } else {
-//   deliveryCountry = deliveryCountry[0].toUpperCase() + deliveryCountry.slice(1).toLowerCase();
+  deliveryCountry = deliveryCountry.toLowerCase();
 
-  switch (deliveryCountry.toLowerCase()) {
-    case countryChina.toLowerCase():
-      alert(`Доставка в ${countryChina} будет стоить ${priceChina}`);
+  switch (deliveryCountry) {
+    case countryChina:
+      alert(`Доставка в ${countryChina[0].toUpperCase() + countryChina.slice(1)} будет стоить ${priceChina} кредитов.`);
       break;
-    case countryChili.toLowerCase():
-      alert(`Доставка в ${countryChili} будет стоить ${priceChili}`);
+    case countryChili:
+      alert(`Доставка в ${countryChili[0].toUpperCase() + countryChili.slice(1)} будет стоить ${priceChili} кредитов.`);
       break;
-    case countryAustralia.toLowerCase():
-      alert(`Доставка в ${countryAustralia} будет стоить ${priceAustralia}`);
+    case countryAustralia:
+      alert(`Доставка в ${countryAustralia[0].toUpperCase() + countryAustralia.slice(1)} будет стоить ${priceAustralia} кредитов.`);
       break;
-    case countryIndia.toLowerCase():
-      alert(`Доставка в ${countryIndia} будет стоить ${priceIndia}`);
+    case countryIndia:
+      alert(`Доставка в ${countryIndia[0].toUpperCase() + countryIndia.slice(1)} будет стоить ${priceIndia} кредитов.`);
       break;
-    case countryJamaica.toLowerCase():
-      alert(`Доставка в ${countryJamaica} будет стоить ${priceJamaica}`);
-      break;
-    case null:
-      alert(`Выбор за вами!`);
+    case countryJamaica:
+      alert(`Доставка в ${countryJamaica[0].toUpperCase() + countryJamaica.slice(1)} будет стоить ${priceJamaica} кредитов.`);
       break;
     default:
       alert(`В вашей стране доставка недоступна!`);
