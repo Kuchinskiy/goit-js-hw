ae1c67f3710006d0b48024ee114008a50db76e6f
Sergey <domarch388640@gmail.com>
2019-08-13 14:11:26 +0300

    commit

---
+++ moduleFirst.js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// const arr = [12, 7, 9, 34, 56];
+
+// let total = 0;
+
+// for (let key of arr) {
+//     console.log(key);
+//     total += key;
+// }
+// console.log(total);
--- moduleFirst.js/task06.js
+++ moduleFirst.js/task06.js
@@ -1,2 +1,11 @@
 'use strict';
 
+let input;
+let total = 0;
+
+do {
+    input = prompt('Введите число');
+    total = total + Number(input);
+} while (input !== null);
+
+alert(total);
