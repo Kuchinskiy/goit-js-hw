6020583fbf293d2d7359a75fae836cb1475fd771
Sergey <domarch388640@gmail.com>
2019-08-14 16:08:25 +0300

    well done

--- moduleFirst.js/task04.js
+++ moduleFirst.js/task04.js
@@ -1,20 +1,23 @@
-'use strict';
+"use strict";
 
 const credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
 
-let howMuchIs = prompt('Сколько дроидов вы готовы купить?');
+let howMuchIs = prompt("Сколько дроидов вы готовы купить?");
 
 if (howMuchIs === null) {
-    console.log('Отменено пользователем!');
+  console.log("Отменено пользователем!");
 } else if (Number.isNaN(Number(howMuchIs))) {
-    console.log('Извините, вы не выбрали количество!');
+  console.log("Извините, вы не выбрали количество!");
 } else {
-    totalPrice = Number(howMuchIs) * pricePerDroid;
-    if (totalPrice > credits) {
-        console.log('Недостаточно средств на счету!');
-    } else {
-        console.log(`Вы купили ${howMuchIs} дроидов, на счету осталось ${credits - totalPrice} кредитов.`)
-    }
-} 
+  totalPrice = Number(howMuchIs) * pricePerDroid;
+  if (totalPrice > credits) {
+    console.log("Недостаточно средств на счету!");
+  } else {
+    console.log(
+      `Вы купили ${howMuchIs} дроидов, на счету осталось ${credits -
+        totalPrice} кредитов.`
+    );
+  }
+}
--- moduleFirst.js/task05.js
+++ moduleFirst.js/task05.js
@@ -22,19 +22,34 @@ if (deliveryCountry === null) {
 
   switch (deliveryCountry) {
     case countryChina:
-      alert(`Доставка в ${countryChina[0].toUpperCase() + countryChina.slice(1)} будет стоить ${priceChina} кредитов.`);
+      alert(
+        `Доставка в ${countryChina[0].toUpperCase() +
+          countryChina.slice(1)} будет стоить ${priceChina} кредитов.`
+      );
       break;
     case countryChili:
-      alert(`Доставка в ${countryChili[0].toUpperCase() + countryChili.slice(1)} будет стоить ${priceChili} кредитов.`);
+      alert(
+        `Доставка в ${countryChili[0].toUpperCase() +
+          countryChili.slice(1)} будет стоить ${priceChili} кредитов.`
+      );
       break;
     case countryAustralia:
-      alert(`Доставка в ${countryAustralia[0].toUpperCase() + countryAustralia.slice(1)} будет стоить ${priceAustralia} кредитов.`);
+      alert(
+        `Доставка в ${countryAustralia[0].toUpperCase() +
+          countryAustralia.slice(1)} будет стоить ${priceAustralia} кредитов.`
+      );
       break;
     case countryIndia:
-      alert(`Доставка в ${countryIndia[0].toUpperCase() + countryIndia.slice(1)} будет стоить ${priceIndia} кредитов.`);
+      alert(
+        `Доставка в ${countryIndia[0].toUpperCase() +
+          countryIndia.slice(1)} будет стоить ${priceIndia} кредитов.`
+      );
       break;
     case countryJamaica:
-      alert(`Доставка в ${countryJamaica[0].toUpperCase() + countryJamaica.slice(1)} будет стоить ${priceJamaica} кредитов.`);
+      alert(
+        `Доставка в ${countryJamaica[0].toUpperCase() +
+          countryJamaica.slice(1)} будет стоить ${priceJamaica} кредитов.`
+      );
       break;
     default:
       alert(`В вашей стране доставка недоступна!`);
