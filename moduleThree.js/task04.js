327c793d31b9b49ad9ca5bdb1b009c90cdc8eb34
Sergey <domarch388640@gmail.com>
2019-08-21 13:39:29 +0300

    commit

--- moduleThree.js/task04.js
+++ moduleThree.js/task04.js
@@ -1,1 +1,36 @@
 'use strict';
+
+/*
+Напиши функцию countTotalSalary(employees) принимающую объект зарплат. 
+Функция считает общую сумму запрплаты работников и возращает ее. 
+Каждое поле объекта, передаваемого в функцию, имеет вид "имя":"зарплата".
+*/
+
+const countTotalSalary = employees => {
+    const Arremployees = Object.values(employees);
+  let summSalary = 0;
+  for (const Salary of Arremployees) {
+    summSalary += Salary;
+}
+return summSalary;
+};
+
+// Вызовы функции для проверки:
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
--- moduleThree.js/task05.js
+++ moduleThree.js/task05.js
@@ -1,1 +1,36 @@
-'use strict';
+"use strict";
+
+/*
+Напиши функцию getAllPropValues(arr, prop),
+которая получает массив объектов и имя ключа. 
+Возвращает массив значений определенного поля 'prop' 
+из каждого объекта в массиве.
+*/
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const getAllPropValues = function(products, key) {
+  const values = [];
+
+  for (const product of products) {
+    // console.log(product[key]);
+
+    if (key in product) {
+      values.push(product[key]);
+    }
+  }
+  return values;
+};
+
+// Вызовы функции для проверки:
+
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
