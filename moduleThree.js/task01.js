873e60af61ccdb93b602d0e0e5242fedb599b26a
Sergey <domarch388640@gmail.com>
2019-08-21 15:40:06 +0300

    commit

--- moduleThree.js/task01.js
+++ moduleThree.js/task01.js
@@ -1,5 +1,29 @@
-'use strict';
+"use strict";
 
+/*
+Напиши скрипт, который, для объекта user, последовательно:
 
+-- добавляет поле mood со значением 'happy'
+-- заменяет значение hobby на 'javascript'
+-- заменяет значение premium на false
+-- выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+*/
 
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true
+};
 
+const listObj = function(obj) {
+  for (let key of Object.keys(obj)) {
+    console.log(`${key}: ${obj[key]}`);
+  }
+};
+
+user.mood = "happy";
+user.hobby = "javascript";
+user.premium = false;
+
+listObj(user);
--- moduleThree.js/task02.js
+++ moduleThree.js/task02.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/*
+Напиши функцию countProps(obj), считающую кол-во свойств в объекте. 
+Функция возвращает число - количество свойств.
+*/
+
+const countProps = (obj) => {
+    let counter = 0;
+    for (var key in obj) {
+      counter++;
+    }
+    return `Всего свойств: ${counter}`;
+};
+
+// Вызовы функции для проверки:
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
