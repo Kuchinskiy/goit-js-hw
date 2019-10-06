9a0fb59596c867959c3e3b5aa9f1139da0ed3947
Sergey <domarch388640@gmail.com>
2019-10-06 13:58:43 +0300

    completed

--- module-02.js/task02.js
+++ module-02.js/task02.js
@@ -1,9 +1,33 @@
-'use strict';
+"use strict";
 
 /*
-* Напиши скрипт подсчета стоимости гравировки украшений. 
-* Для этого создай функцию calculateEngravingPrice(message, pricePerWord) 
+* Напиши скрипт подсчета стоимости гравировки украшений.
+
+* Для этого создай функцию calculateEngravingPrice(string, pricePerWord) 
 * принимающую строку (в строке будут только слова и пробелы) и цену гравировки одного слова,
 * и возвращающую цену гравировки.
 */
 
+const calculateEngravingPrice = function(string, pricePerWord = 40) {
+  const words = string.split(" ");
+  const total = words.length * pricePerWord;
+
+  return total;
+};
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(calculateEngravingPrice("Donec orci lectus aliquam est")); // 200 параметр по умолчанию -->> (40) 
+console.log(calculateEngravingPrice("Donec orci lectus aliquam est", 20)); // 100
