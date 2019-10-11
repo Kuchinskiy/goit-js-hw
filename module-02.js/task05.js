fcc9756336d7deb642efcc829a94853c51626463
Sergey <domarch388640@gmail.com>
2019-10-11 12:18:30 +0300

    completed

--- module-02.js/task05.js
+++ module-02.js/task05.js
@@ -1,9 +1,29 @@
-'use strict';
+"use strict";
 
 /*
-* Напиши функцию checkForSpam(str), принимающую 1 параметр string - строку.
-* Функция проверяет ее на содержание слов spam и sale.
-* Если нашли зарещенное слово то функция возвращает true, 
-* если запрещенных слов нет функция возвращает false. 
-* Слова в строке могут быть в произвольном регистре.
-*/
+ * Напиши функцию checkForSpam(str), принимающую 1 параметр string - строку.
+ * Функция проверяет ее на содержание слов "spam" и "sale".
+ * Если нашли запрещенное слово то функция возвращает true,
+ * если запрещенных слов нет функция возвращает false.
+ * Слова в строке могут быть в произвольном регистре.
+ */
+
+const checkForSpam = str => {
+  if (
+    str.toLowerCase().includes('sale') ||
+    str.toLowerCase().includes('spam')
+  ) {
+    return true;
+  }
+  return false;
+};
+
+// Вызовы функции для проверки:
+
+console.log(checkForSpam("Latest technology news")); // false
+
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+console.log(checkForSpam("Get best sale offers now!")); // true
+
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
--- module-02.js/task06.js
+++ module-02.js/task06.js
@@ -1,21 +1,2 @@
 "use strict";
 
-function checkForSpam(str) {
-  if (
-    str.toLowerCase().includes("spam") ||
-    str.toLowerCase().includes("sale")
-  ) {
-    return true;
-  }
-  return false;
-}
-
-// Вызовы функции для проверки:
-
-console.log(checkForSpam("Latest technology news")); // false
-
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-
-console.log(checkForSpam("Get best sale offers now!")); // true
-
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
