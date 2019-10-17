1f491915c65c5ecf291fd99b4f1cd251938cd308
Sergey <domarch388640@gmail.com>
2019-10-17 18:29:58 +0300

    completed

--- module-02.js/task01.js
+++ module-02.js/task01.js
@@ -15,35 +15,37 @@
 * -->>-- logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 */
 
-// Вариант-1 
-// -->> function expression
+// Вариант-1
 
-const logItems = function(array) {
-  let num = 1;
-  const users = ["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"];
-  for (let j = 0; j < users.length; j++) {
-    console.log(`${num} - ${users[j]}`);
-    num += 1;
-  }
-  return array;
-};
-
-// Вызовы функции для проверки:
-
-logItems();
-logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
-
-
-// Вариант-2
-// -->> function declaration
-
-function logItems (array) {
-    for (let j = 0; j < array.length; j++) {
+const logItems = (array) => {
+    for (let j = 0; j < array.length; j += 1) {
         console.log(`${j + 1} - ${array[j]}`);
     }
-}
+};
 
 // Вызовы функции для проверки:
 
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+
+
+// Вариант-2
+
+// const logItems = (array) => {
+//   let str = '';
+//   for (let j = 0; j < array.length; j += 1) {
+//     str += `${j + 1} - ${users[j]} \n`;  // -->> array - 1;
+
+//     str += `${j + 1} - ${numbers[j]} \n`;  // -->> array - 2;
+//   }
+//   return str;
+// };
+
+// Вызовы функции для проверки:
+
+// const users = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];  // -->> arr-1;
+// console.log(logItems(users));
+
+// const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];  // -->> arr-2;
+// console.log(logItems(numbers));
