781d3477e7bff967f4c47dad55605c4160bd5c24
Sergey <domarch388640@gmail.com>
2019-11-29 17:00:58 +0200

    completed

--- module-06.js/task-06.js
+++ module-06.js/task-06.js
@@ -1,3 +1,15 @@
-'use strict';
+"use strict";
 
-import users from './users.js';
+import users from "./users.js";
+
+/*
+ * Получить массив пользователей попадающих под возрастную категорию от min до max лет (поле age).
+ */
+
+const getUsersWithAge = (users, min, max) =>
+  users.filter(user => user.age >= min && user.age < max);
+
+console.log(getUsersWithAge(users, 20, 30));
+// [object {Ross Vazquez}, object {Elma Head}, object {Carey Barr}]
+console.log(getUsersWithAge(users, 30, 40));
+// [object {Moore Hensley}, object {Sharlene Bush}, object {Blackburn Dotson}, object {Sheree Anthony}]
