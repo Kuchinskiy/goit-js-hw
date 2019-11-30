433046ff2c2c4347cc13f57540b68503fff8d4d7
Sergey <domarch388640@gmail.com>
2019-11-30 13:30:48 +0200

    completed

--- module-06.js/task-10.js
+++ module-06.js/task-10.js
@@ -1,3 +1,24 @@
-'use strict';
+"use strict";
 
-import users from './users.js';
+import users from "./users.js";
+
+/*
+ * Получить массив всех умений пользователей (поле skills),
+ * при этом не должно быть повторяющихся навыков и они должны быть отсортированы в алфавитном порядке.
+ */
+
+const getSortedUniqueSkills = users => {
+  const arr = [];
+  users
+    .reduce((allSkills, { skills }) => [...allSkills, ...skills], [])
+    .forEach(element => {
+      if (!arr.includes(element)) {
+        arr.push(element);
+      }
+    });
+  return arr.sort();
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum',
+//  'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
