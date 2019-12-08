c55c49f81ec94e48ec7418aca45a8baf13046d0a
Sergey <domarch388640@gmail.com>
2019-12-08 15:18:45 +0200

    completed

--- module-07.js/main.js
+++ module-07.js/main.js
@@ -1,46 +1,70 @@
 'use strict';
 
-const btn = document.querySelector('.cool-btn');
-btn.addEventListener('click', () => {
-    console.log('clicked');
-});
+// const btn = document.querySelector('.cool-btn');
+// btn.addEventListener('click', () => {
+//     console.log('clicked');
+// });
 
 
-const gameContainer = document.querySelector('.game');
-const square = document.createElement('div');
-const step = '20px';
-const squarePosition = {
-    x: 0,
-    y: 0
-}
+// const gameContainer = document.querySelector('.game');
+// const square = document.createElement('div');
+// const step = '20px';
+// const squarePosition = {
+//     x: 0,
+//     y: 0
+// }
 
-square.style.width = '20px';
-square.style.height = '20px';
-square.style.background = 'purple';
+// square.style.width = '20px';
+// square.style.height = '20px';
+// square.style.background = 'purple';
 
-gameContainer.appendChild(square);
+// gameContainer.appendChild(square);
 
 
-const addMotionController = element => {
-    document.addEventListener ('keydown', (evt) => {
-        const keyCode = evt.keyCode;
-        if (keyCode === 40) {
-            squarePosition.y = squarePosition.y + 20;
-            element.style.marginTop = `${squarePosition.y}px`;
-        }
-        if (keyCode === 38) {
-            squarePosition.y = squarePosition.y - 20;
-            element.style.marginTop = `${squarePosition.y}px`;
-        }
-        if (keyCode === 37) {
-            squarePosition.x = squarePosition.x - 20;
-            element.style.marginLeft = `${squarePosition.x}px`;
-        }
-        if (keyCode === 39) {
-            squarePosition.x = squarePosition.x + 20;
-            element.style.marginLeft = `${squarePosition.x}px`;
-        }
-    })
-}
+// const addMotionController = element => {
+//     document.addEventListener ('keydown', (evt) => {
+//         const keyCode = evt.keyCode;
+//         if (keyCode === 40) {
+//             squarePosition.y = squarePosition.y + 20;
+//             element.style.marginTop = `${squarePosition.y}px`;
+//         }
+//         if (keyCode === 38) {
+//             squarePosition.y = squarePosition.y - 20;
+//             element.style.marginTop = `${squarePosition.y}px`;
+//         }
+//         if (keyCode === 37) {
+//             squarePosition.x = squarePosition.x - 20;
+//             element.style.marginLeft = `${squarePosition.x}px`;
+//         }
+//         if (keyCode === 39) {
+//             squarePosition.x = squarePosition.x + 20;
+//             element.style.marginLeft = `${squarePosition.x}px`;
+//         }
+//     })
+// }
 
-addMotionController(square);
+// addMotionController(square);
+
+
+
+// ----*smallPracticeAnimation----
+
+// function myMove() {
+//     const elem = document.getElementById("persone");   
+//     let pos = 0;
+//     let id = setInterval(frame, 15);
+//     function frame() {
+//       if (pos === 270) {
+//         clearInterval(id);
+//       } else {
+//         pos++; 
+//         elem.style.top = pos + 'px'; 
+//         elem.style.left = pos + 'px'; 
+//       }
+//     }
+//   }
+
+// setInterval(myMove, 3000);
+
+
+
