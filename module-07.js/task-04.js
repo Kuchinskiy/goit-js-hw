98019c0cd86a707d5db2d511db2d41ed9fc2a2b3
Sergey <domarch388640@gmail.com>
2020-02-02 15:19:40 +0200

    completed

--- module-07.js/task-04.js
+++ module-07.js/task-04.js
@@ -9,6 +9,7 @@
 * Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса/
 */
 
+// Вариант-1(через class)
 const valueById = document.querySelector("#value");
 let counterValue = 0;
 
@@ -21,3 +22,22 @@ document.querySelector(".btn-decrement").addEventListener("click", () => {
   counterValue -= 1;
   valueById.textContent = counterValue;
 });
+
+
+// Вариант-2(через data-атрибуты)
+const valueByIdSpan = document.querySelector("#value");
+let counterValueClick = 0;
+
+document
+  .querySelector('#counter button[data-action="increment"]')
+  .addEventListener("click", () => {
+    counterValueClick += 1;
+    valueByIdSpan.textContent = counterValueClick;
+  });
+
+document
+  .querySelector('#counter button[data-action="decrement"]')
+  .addEventListener("click", () => {
+    counterValueClick -= 1;
+    valueByIdSpan.textContent = counterValueClick;
+  });
