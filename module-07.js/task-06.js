3b07993f7ec2b5380bd14e6a5a3453a2e9bfc667
Sergey <domarch388640@gmail.com>
2020-02-02 23:45:48 +0200

    completed

---
+++ module-07.js/task-06.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*
+* Напиши скрипт, который бы при потере фокуса на инпуте, 
+* проверял его содержимое на правильное количество символов.
+
+* Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+* Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
+
+* Для добавления стилей, используй CSS-классы valid и invalid.
+*/
+
+const inputById = document.querySelector("#validation-input");
+
+inputById.addEventListener("blur", () => {
+  const setLengthCharacters = Number(inputById.dataset.length);
+  let EnteredQuantity = inputById.value.length;
+
+  if (setLengthCharacters === EnteredQuantity) {
+    inputById.classList.add("valid");
+    inputById.classList.remove("invalid");
+  } else {
+    inputById.classList.add("invalid");
+  }
+});
